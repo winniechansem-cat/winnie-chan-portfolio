3f19dce3867c7c0b9ad6ccd3586bd4d14bbdfa79
winniechansem-cat <winniechan.sem@gmail.com>
2026-01-28 14:04:48 +1100

    Format rendering of App component in index.tsx

--- index.tsx
+++ index.tsx
@@ -5,5 +5,9 @@ import App from './App';
 const rootElement = document.getElementById('root');
 if (rootElement) {
     const root = ReactDOM.createRoot(rootElement);
-    root.render(<React.StrictMode><App /></React.StrictMode>);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
 }
